eb65a2b9e66bb0798b4517b012a87b615d002f65
Guilherme Luiz <guiiluiz44@gmail.com>
2020-02-05 17:21:34 -0300

    create login form and style

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import '../style/Login.css';
 
+function handleSubmit(event, setRedirect) {
+  event.preventDefault();
+  setRedirect(true)
+}
+
 function Login() {
+  const [redirect, setRedirect] = useState(false);
+  if (redirect) return <Redirect to="/recipes" />
   return (
-    <div>
-      <form className="login-content">
-        <input type="email" placeholder="Digite seu Email" />
-        <input type="password" placeholder="Digite sua Senha" />
+    <div className="login">
+      <h1>Login</h1>
+      <form onSubmit={(event) => handleSubmit(event, setRedirect)} className="login-content">
+        <input
+          className="form-input"
+          required
+          type="email"
+          placeholder="Digite seu Email"
+        />
+        <input
+          className="form-input"
+          required
+          minLength="6"
+          type="password"
+          placeholder="Digite sua Senha"
+        />
+        <input className="form-submit" type="submit" value="Entrar" />
       </form>
     </div>
   );
