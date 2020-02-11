c94fd800823ae90c6d87edd92d4dba9aab8b2d0f
Guilherme Luiz <guiiluiz44@gmail.com>
2020-02-11 20:24:35 -0300

    fix codeclimate errors

--- src/services/APIs.js
+++ src/services/APIs.js
@@ -5,15 +5,16 @@ const getRecipes = async (type, endPoint) => {
 
 export const getRandomRecipes = async (type, number) => {
   const randomRecipes = [];
-  for (let index = 0; index < number; index++) {
+  for (let index = 0; index < number; index += 1) {
     const response = await fetch(`https://www.the${type}db.com/api/json/v1/1/random.php`).then((data) => data.json());
     let recipe;
     if (type === 'meal') recipe = response.meals[0];
     else recipe = response.drinks[0];
     randomRecipes.push(recipe);
   }
+  console.log(randomRecipes)
   return randomRecipes;
-}
+};
 
 export const getIngredientImage = async (type, endPoint) => {
   const src = `https://www.the${type}db.com/images/ingredients/${endPoint}`;
@@ -28,14 +29,14 @@ export const getRecipeImage = async (type, endPoint) => {
 export const searchByName = (type, parameter) => {
   const endPoint = `search.php?s=${parameter}`;
   return getRecipes(type, endPoint);
-}
+};
 
 export const searchByIngredient = (type, parameter) => {
   const endPoint = `filter.php?i=${parameter}`;
   return getRecipes(type, endPoint);
-}
+};
 
 export const searchByFirstLetter = (type, parameter) => {
   const endPoint = `search.php?f=${parameter}`;
   return getRecipes(type, endPoint);
-}
+};
