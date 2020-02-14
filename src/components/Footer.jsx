f613203e268c08d439f1eb92b5913136a7d9589d
Conrado Medeiros <conradodemedeiros@hotmail.com>
2020-02-14 18:03:58 -0300

    code climate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import explorer from '../img/explorer.svg'
-import food from '../img/food.svg'
-import drink from '../img/drink.svg'
+import explorer from '../img/explorer.svg';
+import food from '../img/food.svg';
+import drink from '../img/drink.svg';
 import '../style/Footer.css';
-
 
 export default function Footer() {
   return (
     <div className="content-footer">
-      <ul className="footer" >
-        <Link to="/explorer"><li><span><img className="img-footer" src={explorer} alt="explorer"/></span></li></Link>
-        <Link to="/food"><li><span><img className="img-footer" src={food} alt="food"/></span></li></Link>
-        <Link to="/drink"><li><span><img className="img-footer" src={drink} alt="drink"/></span></li></Link>
+      <ul className="footer">
+        <Link to="/explorer">
+          <li>
+            <span>
+              <img className="img-footer" src={explorer} alt="explorer" />
+            </span>
+          </li>
+        </Link>
+        <Link to="/food">
+          <li>
+            <span>
+              <img className="img-footer" src={food} alt="food" />
+            </span>
+          </li>
+        </Link>
+        <Link to="/drink">
+          <li>
+            <span>
+              <img className="img-footer" src={drink} alt="drink" />
+            </span>
+          </li>
+        </Link>
       </ul>
     </div>
   );
 }
-
-  
