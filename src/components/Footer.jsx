956d5bd2be3aa196c0b60da15d0e5fede6cd1466
Conrado Medeiros <conradodemedeiros@hotmail.com>
2020-02-14 15:16:15 -0300

    finalizando footter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,18 @@
-// ainda será feito
+import React from 'react'
+import { Link } from 'react-router-dom';
+import explorer from '../img/explorer.svg'
+import food from '../img/food.svg'
+import drink from '../img/drink.svg'
+
+
+export default function Footer() {
+  return (
+    <ul>
+      <Link to="/explorer"><li><button><img src={explorer} alt="explorer"/></button></li></Link>
+      <Link to="/food"><li><button><img src={food} alt="food"/></button></li></Link>
+      <Link to="/drink"><li><button><img src={drink} alt="drink"/></button></li></Link>
+    </ul>
+  );
+}
+
+  
