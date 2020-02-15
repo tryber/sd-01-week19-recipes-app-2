43bd18f15944ff99b40568bc272e903f62a44348
Conrado Medeiros <conradodemedeiros@hotmail.com>
2020-02-15 16:56:47 -0300

    atualização

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,31 +5,25 @@ import food from '../img/food.svg';
 import drink from '../img/drink.svg';
 import '../style/Footer.css';
 
+const generateLiFooter = (link, img) => {
+  return (
+  <li>
+    <Link to={`/${link}`}>
+      <span>
+        <img className="img-footer" src={img} alt={img} />
+      </span>
+    </Link>
+  </li>
+  );
+}
+
 export default function Footer() {
   return (
     <div className="content-footer">
       <ul className="footer">
-        <li>
-          <Link to="/explorer">
-            <span>
-              <img className="img-footer" src={explorer} alt="explorer" />
-            </span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/food">
-            <span>
-              <img className="img-footer" src={food} alt="food" />
-            </span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/drink">
-            <span>
-              <img className="img-footer" src={drink} alt="drink" />
-            </span>
-          </Link>
-        </li>
+        {generateLiFooter("explorer", explorer)}
+        {generateLiFooter("food", food)}
+        {generateLiFooter("drink", drink)}
       </ul>
     </div>
   );
