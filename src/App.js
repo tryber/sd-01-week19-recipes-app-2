9a10b1da67fb28c4fd1612443e2bf0018b9638f4
Mateus Talles <mateustalles@gmail.com>
2020-02-19 00:20:48 -0300

    merge

--- src/App.js
+++ src/App.js
@@ -6,13 +6,10 @@ import {
 } from 'react-router-dom';
 import AppProvider from './context/AppContext';
 import Login from './pages/Login';
-<<<<<<< HEAD
 import Header from './components/Header';
 import './style/App.css';
-=======
 import Explorar from './pages/Explorar';
 import ExplorarReceitas from './pages/ExplorarReceitas';
->>>>>>> 4226065f7eaee80dd6591e09a617a8954075b853
 import MainRecipes from './pages/MainRecipes';
 import Profile from './pages/Profile';
 import RecipeDetails from './pages/RecipeDetails';
